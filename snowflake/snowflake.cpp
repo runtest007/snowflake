cbddce5017ed012387d527a2756f059d4e45e367
sky_walker <897222281@qq.com>
2020-02-01 12:06:56 +0800

    Add files via upload

---
+++ snowflake/snowflake.cpp
@@ -0,0 +1,83 @@
+#include "snowflake.h"
+
+/*
+默认情况下 机器码和线程id一共占用10位，剩余12位产生序列号，那么1毫秒的线程可以建立4096个连接足够用了，最低(使用7位)每毫秒需产生128个序列号
+即最低可以为单线程每秒提供12.8w的新建连接序列号
+|0|00000000000000000000000000000000000000000|0000000000|000000000000|
+*/
+CSnowFlakeGenerator::CSnowFlakeGenerator(u_int16_t MachineBitNum,u_int16_t ThreadBitNum,u_int16_t ThreadId,u_int16_t MachineCode)
+{
+	u16MachineBitNum = MachineBitNum;
+	u16ThreadBitNum = ThreadBitNum;
+
+	u64ThreadId = ThreadId;
+	u64MachineCode = MachineCode;
+
+	u16RemainBit = REMAIN_BIT;
+
+	if((u16MachineBitNum+u16ThreadBitNum)>(u16RemainBit-MIN_SEQ_BIT)){
+		LOGGER_INFO(formatStr("machine code and thread id is bigger"));
+		u16MachineBitNum = DEFAULT_MACHINE_BIT;
+		u16ThreadBitNum = DEFAULT_THREAD_BIT;
+	}
+
+	
+	i64StartTimeStamp = DEFAULT_TIME_SET;
+	
+	i64LastTimeStamp = 0;
+
+	/*序列号所使用的位数*/
+	u_int16_t seqbit = u16RemainBit - u16MachineBitNum - u16ThreadBitNum;
+	u64MaxSeq = (1<<seqbit) -1;
+
+	/*机器码偏移位*/
+	u16MachineOffset = seqbit;
+
+	/*线程ID偏移*/
+	u16ThreadOffset = seqbit + 	u16MachineBitNum;
+
+	/*时间戳偏移*/
+	u16TimeStampOffset = seqbit + u16ThreadBitNum + u16MachineBitNum;
+
+	u64Sequence = 0;
+
+
+	
+}
+
+int64_t CSnowFlakeGenerator::GetNextId()
+{
+	int64_t i64CurrentMillSec = 0;
+	int64_t retval=0;
+	
+	i64CurrentMillSec=GetMillSecTime();
+	if(i64CurrentMillSec < i64LastTimeStamp){
+		LOGGER_ERROR(formatStr("error time stamp create!!"));
+		return -1;
+	}
+	
+	/*如果当前毫秒时间等于上次毫秒时间 则增加序列号*/
+	if(i64CurrentMillSec == i64LastTimeStamp){
+		i64LastTimeStamp = i64CurrentMillSec;
+		u64Sequence ++;
+		if(u64Sequence >= u64MaxSeq){
+			GetNextMillSecTime();
+		}
+		
+	}else{
+		//不等于上次毫秒时间则seq 置0
+		i64LastTimeStamp = i64CurrentMillSec;
+		u64Sequence = 0;
+	}
+	//printf("=====%u====%u====%u===%u===",u16TimeStampOffset,u16ThreadOffset,u16MachineOffset,u64Sequence);
+
+	//printf("x1:%d====x2:%u====x3:%u===\n",i64CurrentMillSec-i64StartTimeStamp,u64ThreadId,u64MachineCode);
+	retval = ((i64CurrentMillSec-i64StartTimeStamp)<<u16TimeStampOffset) | \
+		u64ThreadId<< u16ThreadOffset | u64MachineCode<<u16MachineOffset | u64Sequence;
+
+	//printf("================return val:%ld====\n",retval);
+	return retval;
+
+}
+
+
---
+++ snowflake/snowflake.h
@@ -0,0 +1,75 @@
+#ifndef __SNOWFLAKE__H
+#define __SNOWFLAKE__H
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <linux/unistd.h>
+#include <errno.h>
+#include<linux/types.h>
+#include <sys/time.h>
+
+#include "basetype.h"
+#include "logger.h"
+
+
+
+#define REMAIN_BIT 22
+#define MIN_SEQ_BIT 7
+#define DEFAULT_MACHINE_BIT 5
+#define DEFAULT_THREAD_BIT 5
+
+
+#define GENERATOR_ID_ERROR -1
+#define GENERATOR_ID_DEFAULT 0
+
+#define DEFAULT_TIME_SET 1555322400
+
+
+
+class CSnowFlakeGenerator{
+	private:
+		u_int16_t u16MachineBitNum;  	//机器位数 max 31
+		u_int16_t u16ThreadBitNum;   	//线程位数	 max 31
+		
+		u_int64_t u64ThreadId;			//线程ID 为了移位定义成u64
+		u_int64_t u64MachineCode;		//机器码 为了移位定义成u64
+		
+		u_int16_t u16ThreadOffset;		//线程偏移量
+		u_int16_t u16MachineOffset;		//机器码便宜量
+		u_int16_t u16TimeStampOffset;	//时间戳偏移量
+
+		u_int16_t u16RemainBit;			//预留位数,64 位中第一位不使用，41 位为毫秒级时间戳，后22位为预留给机器码和线程id以及产生的序列号使用
+		
+		u_int64_t u64MaxSeq;			//最大序列值
+		int64_t i64StartTimeStamp;		//过去的某个时间戳[2019/04/15/18:00:00]
+		u_int64_t u64Sequence;			//当前序号
+
+		int64_t i64LastTimeStamp;		//上次时间戳
+	private:
+		int64_t GetMillSecTime(){
+			struct timeval tmval;
+			gettimeofday(&tmval,NULL);
+			return (tmval.tv_sec*1000 + tmval.tv_usec/1000);
+		}
+
+		int64_t GetNextMillSecTime()
+		{
+			//等待下一毫秒出现
+			while(1)
+			{
+				if(i64LastTimeStamp < GetMillSecTime())
+				{
+					i64LastTimeStamp = GetMillSecTime();
+					break;
+				}
+			}
+		}
+
+	public:
+		CSnowFlakeGenerator(u_int16_t MachineBitNum,u_int16_t ThreadBitNum,u_int16_t ThreadId,u_int16_t MachineCode);
+		int64_t GetNextId();
+		
+};
+
+#endif
